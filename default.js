37e4e5f4ef5180d59e5eeddbe801c453f339d97d
kazikai <junghyun.han@sk.com>
2015-02-04 15:44:22 +0900

    responsive design 1025 width develop

--- default.js
+++ default.js
@@ -3,5 +3,7 @@ $( function () {
     $( ".gnb>button" ).on( "click", function(){
         var $this = $( this );
         $this.next().slideToggle(200);
-    })
+    }), $( ".gnb>ul>li>a" ).on( "mouseover focus", function() {
+        $( this ).parent().addClass( "active" ).siblings().removeClass( "active" );
+    });
 });
